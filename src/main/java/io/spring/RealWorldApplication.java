49489faf76d0d9f6f88deefa1978028b3989ae6f
zhangbose <129360131+zhangbose@users.noreply.github.com>
2024-03-31 13:37:48 +0800

    Update RealWorldApplication.java

--- src/main/java/io/spring/RealWorldApplication.java
+++ src/main/java/io/spring/RealWorldApplication.java
@@ -2,11 +2,14 @@ package io.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.spring.core.article.Article;
 
 @SpringBootApplication
 public class RealWorldApplication {
 
   public static void main(String[] args) {
+    Article article = new Article("123", "456", "body");
+    article.doSomeTasks("title", "desc", "body");
     SpringApplication.run(RealWorldApplication.class, args);
   }
 }
